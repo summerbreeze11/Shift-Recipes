ac0189b8eee7227e1c3107bef6b7c6344b9669ad
summerbreeze11 <wf992211@mail.ru>
2023-07-06 01:48:14 +0700

    Добавил главное меню

--- app/src/main/java/com/summerbreeze11/shift_summer/MainActivity.kt
+++ app/src/main/java/com/summerbreeze11/shift_summer/MainActivity.kt
@@ -2,10 +2,33 @@ package com.summerbreeze11.shift_summer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.summerbreeze11.shift_summer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        onBottomNavClick()
+    }
+
+    private fun onBottomNavClick() {
+        binding.bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.create_rec -> {
+                    Toast.makeText(this, "create_rec", Toast.LENGTH_SHORT).show()
+                }
+                R.id.ready_rec -> {
+                    Toast.makeText(this, "ready_rec", Toast.LENGTH_SHORT).show()
+                }
+                R.id.saved_rec -> {
+                    Toast.makeText(this, "saved_rec", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
